d96a67c145040cf52b8f35767ffa9f9f5450b8e5
Oliver Salzburg <oliver.salzburg@gmail.com>
2010-06-16 23:09:46 +0000

    Expanded demo application.
    git-svn-id: https://svn.code.sf.net/p/taskdialognet/code/trunk@26 d11d1fa2-0103-4124-8555-2c878f2140d0

--- TaskDialogDemo/DemoForm.cs
+++ TaskDialogDemo/DemoForm.cs
@@ -16,25 +16,64 @@ namespace TaskDialogDemo {
     }
 
     private void button1_Click( object sender, EventArgs e ) {
-      TaskDialog.ButtonClicked += TaskDialog_ButtonClicked;
+      TaskDialog.ButtonClicked        += TaskDialog_ButtonClicked;
+      TaskDialog.Created              += TaskDialog_Created;
+      TaskDialog.Destroyed            += TaskDialog_Destroyed;
+      TaskDialog.DialogConstructed    += TaskDialog_DialogConstructed;
+      TaskDialog.ExpandoButtonClicked += TaskDialog_ExpandoButtonClicked;
+      TaskDialog.Help                 += TaskDialog_Help;
+      TaskDialog.HyperlinkClicked     += TaskDialog_HyperlinkClicked;
 
-      //This is the so-called verification text. It is quite long to check common button layout.
       TaskDialog.ForceEmulationMode = forceEmulationCheckBox.Checked;
-      DialogResult result = TaskDialog.ShowTaskDialogBox( "title", "mainInstruction", "content", "Do you require additional information regarding this matter?", "footer", "This is the so-called verification text. It is quite long to check common button layout.",
-                                                                     "radio1|radio2", "commandButton1|commandButton2|commandButton3|commandButton4|commandButton4|commandButton4", CommonButtons.Ok | CommonButtons.Cancel | CommonButtons.Close | CommonButtons.No | CommonButtons.Retry, CommonIcon.Information,
-                                                                     CommonIcon.Information, ProgressBarStyle.None );
+      DialogResult result = TaskDialog.ShowTaskDialogBox(
+        "title",
+        "mainInstruction",
+        "content",
+        "Do you require additional information regarding this matter?",
+        "Do you require <A HREF=\"some link\">more information</A>?",
+        "This is the so-called verification text. It is quite long to check common button layout.",
+        "radio1|radio2",
+        "commandButton1|commandButton2|commandButton3|commandButton4|commandButton4|commandButton4",
+        CommonButtons.Ok | CommonButtons.Cancel | CommonButtons.Close | CommonButtons.No | CommonButtons.Retry | CommonButtons.Yes,
+        CommonIcon.Information,
+        CommonIcon.Information,
+        ProgressBarStyle.None );
 
-      TaskDialog.ButtonClicked -= TaskDialog_ButtonClicked;
-      /*
-      TaskDialog.MessageBox( "title", "mainInstruction", "content", "expandedInfo", "footer",
-                                                      "verificationText", CommonButtons.Ok,
-                                                      CommonIcon.Information,
-                                                      CommonIcon.Information, ProgressBarStyle.None );
-      */
+      TaskDialog.ButtonClicked        -= TaskDialog_ButtonClicked;
+      TaskDialog.Created              -= TaskDialog_Created;
+      TaskDialog.Destroyed            -= TaskDialog_Destroyed;
+      TaskDialog.DialogConstructed    -= TaskDialog_DialogConstructed;
+      TaskDialog.ExpandoButtonClicked -= TaskDialog_ExpandoButtonClicked;
+      TaskDialog.Help                 -= TaskDialog_Help;
+      TaskDialog.HyperlinkClicked     -= TaskDialog_HyperlinkClicked;
+    }
+
+    void TaskDialog_HyperlinkClicked( ITaskDialog sender, HyperlinkClickedArgs args ) {
+      Console.WriteLine( "Hyperlink clicked: Link: {0}", args.Url );
+    }
+
+    void TaskDialog_Help( ITaskDialog sender, EventArgs args ) {
+      Console.WriteLine( "Help invoked (F1 pressed)." );
+    }
+
+    void TaskDialog_ExpandoButtonClicked( ITaskDialog sender, ExpandoButtonClickedArgs args ) {
+      Console.WriteLine( "Expando button clicked. Currently expanded: {0}", args.IsExpanded );
+    }
+
+    void TaskDialog_DialogConstructed( ITaskDialog sender, EventArgs args ) {
+      Console.WriteLine( "Dialog constructed." );
+    }
+
+    void TaskDialog_Destroyed( ITaskDialog sender, EventArgs args ) {
+      Console.WriteLine( "Dialog destroyed." );
+    }
+
+    void TaskDialog_Created( ITaskDialog sender, EventArgs args ) {
+      Console.WriteLine( "Dialog created." );
     }
 
     void TaskDialog_ButtonClicked( ITaskDialog sender, ButtonClickedArgs args ) {
-      return;
+      Console.WriteLine( "Button clicked: {0}", args.Id );
     }
   }
 }
